4fabff25e6bbb4fe8f69ed5da86419ba6bb0ab5e
Thek64ok <canndaner@list.ru>
2021-07-17 12:09:38 +0300

    css/

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-    echo 'asd';
+
 ?>
+<html>
+    <head>
+        <title>Укоротитель ссылок</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="/css/signin.css" rel="stylesheet">
+        <style>
+            body{
+                background-color: bisque;
+            }
+        </style>
+    </head>
+    <body class="text-center">
+        <main class="form-signin">
+        <form>
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="Ссылка">
+                <label for="floatingInput">Ссылка</label>
+            </div><br>
+            <button class="btn btn-lg btn-primary" type="submit">Укоротить ссылку</button>
+        </form>
+        </main>
+    </body>
+</html>
