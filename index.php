7a076e2cc3d8e09eec6282dd0770d4b8d5a0ac09
Thek64ok <canndaner@list.ru>
2021-07-17 14:41:41 +0300

    commit on 'master'

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
     session_start();
+    //сессия нужна - не знаю зачем, лишней не будет
+    //выходит так, что в результирующую ссылку, укороченную, я записываю хеш в массив GET
+    //отсюда и работаю, не судите строго...
+    if($_GET['link'] != '')
+    {
+        $hash_link = $_GET['link'];
+        $conn = new PDO("pgsql:dbname=kok;host=localhost", "postgres", "322");
+        //отправляю запрос на получение данных: ссылки и её текущеё колво перевоход по хешу и массиве GET
+        $s = $conn->prepare("SELECT original_link, using_count FROM koks WHERE hash_link = '$hash_link'");
+        $s->execute();
+        $count = $s->rowCount();
+        if($count != 0)
+        {
+            $arr = $s->fetch(PDO::FETCH_LAZY);
+            $link = $arr[0];
+            $counts = $arr[1] + 1;
+            //тут получаю и увеличиваю на один, так как следующий шаг, или скорый, приведет к переходу
+            $s = $conn->prepare("UPDATE koks SET using_count = $counts WHERE hash_link = '$hash_link'");
+            //тут обновил этот параметр и перехожу по ссылке
+            $s->execute();
+            header("Location: $link");
+        }
+    }
+
 ?>
 <html>
     <head>
@@ -19,13 +43,16 @@
         <form method="POST" action="short.php" id="id_form">
             <div class="form-floating">
                 <input type="text" class="form-control" id="floatingInput" placeholder="Ссылка" name="link">
-                <label for="floatingInput">Ссылка</label>
+                <label for="floatingInput" id="id_label">Ссылка</label>
             </div><br>
         </form>
         <button class="btn btn-lg btn-primary" type="submit" id="id_button">Укоротить ссылку</button>
         </main>
         <script>
             $(document).ready(function(){
+                //когда-то научусь обходиться без этого, но пока так
+                //раз у меня кнопка не внутри формы, required я поставить не могу, поэтому так
+                //могу, конечно, создать невидимую кнопку, но работает через раз
                 if($('#floatingInput').val() == '')
                     $('#id_button').prop('disabled', true);
                 
@@ -35,20 +62,39 @@
                     else
                         $('#id_button').prop('disabled', true);
                 })
-
+                //событие на кнопку снизу
                 $('#id_button').click(function(){
-                    $.ajax(
+                    if($(this).text() == 'Скопировать')
                     {
-                        url: 'kok.php',
-                        type: "POST",
-                        dataType: "html",
-                        data: $("#id_form").serialize(),
-                        async: false,
-                        success: function(response){
-                            result = $.parseJSON(response);
-                            console.log(result);
-                        }
-                    });
+                        $('#floatingInput').select();
+                        document.execCommand("copy");
+                    }
+                    else
+                    {
+                        $.ajax(
+                        {
+                            url: 'kok.php',
+                            type: "POST",
+                            dataType: "html",
+                            data: $("#id_form").serialize(),
+                            async: false,
+                            success: function(response){
+                                //тут мы получаем ответ от сервера, проверяем на то что внутри и работаем по сценарию
+                                result = $.parseJSON(response);
+                                if(result != 'error')
+                                {
+                                    $('#id_label').text('Ваша короткая ссылка');
+                                    $('#floatingInput').prop('placeholder', 'Ваша короткая ссылка')
+                                    $('#floatingInput').val(result);
+                                    $('#id_button').text('Скопировать');
+                                }
+                                else
+                                {
+                                    $('#floatingInput').val(result);
+                                }
+                            }
+                        });
+                    }
                 })
             })
         </script>
--- kok.php
+++ kok.php
@@ -1,39 +1,56 @@
 <?php
     session_start();
+    //проверка 1
     $full_link = $_POST['link'];
-    $valid = http_response($full_link);
-    if($valid)
+    if(mb_substr($full_link, 0, 3) == 'kok')
     {
-        $conn = new PDO("pgsql:dbname=kok;host=localhost", "postgres", "322");
-        $s = $conn->prepare("SELECT * FROM koks WHERE original_link = '$full_link'");
-        $s->execute();
-        $count = $s->rowCount();
-        if($count != 0)
+        $message = 'error';
+    }
+    //проверка 1
+    else
+    {
+        //проверка 2
+        $valid = http_response($full_link);
+        //проверка 2, дальше сраборает один из вариантов
+        if($valid)
         {
-            $short_link = $s->fetch(PDO::FETCH_LAZY);
-            $message = $short_link[2];
+            $conn = new PDO("pgsql:dbname=kok;host=localhost", "postgres", "322");
+            $s = $conn->prepare("SELECT * FROM koks WHERE original_link = '$full_link'");
+            $s->execute();
+            $count = $s->rowCount();
+            //проверка 3
+            if($count != 0)
+            {
+                $short_link = $s->fetch(PDO::FETCH_LAZY);
+                $message = $short_link[2];
+            }
+            //проверка 3, опять таки, либо создать, либо даст уже существующую укороченную ссылку
+            else
+            {
+                $s = $conn->prepare("SELECT max(id_link) FROM koks");
+                $s->execute();
+                $main_id = $s->fetch(PDO::FETCH_LAZY);
+                $id = $main_id[0];
+                $hash = mb_substr(md5($full_link), 0, 7);
+                //ссылку я получаю укороченную из хеша и записываю в массив GET, грубо говоря
+                $short_link = "kok.com/?link=$hash";
+                if(is_null($id))
+                {
+                    $s = $conn->prepare("INSERT INTO koks VALUES(1, '$full_link', '$short_link', '$hash', 0)");
+                    //соответственно, если записей вообще нету, то первый параметр равен 1
+                    //эта проверка срабатывает лишь в одном единственном случае
+                }
+                else
+                    $s = $conn->prepare("INSERT INTO koks VALUES($id + 1, '$full_link', '$short_link', '$hash', 0)");
+                $s->execute();
+                $message = $short_link;
+                //по итогу получить переменную $message - там либо error, либо короткая ссылка, все это в ajax формах отдыхает
+            }
         }
         else
         {
-            $s = $conn->prepare("SELECT max(id_link) FROM koks");
-            $s->execute();
-            $main_id = $s->fetch(PDO::FETCH_LAZY);
-            $id = $main_id[0];
-            $hash = mb_substr(md5($full_link), 0, 7);
-            $short_link = "kok.com/$hash";
-            if(is_null($id))
-            {
-                $s = $conn->prepare("INSERT INTO koks VALUES(1, '$full_link', '$short_link', '$hash')");
-            }
-            else
-                $s = $conn->prepare("INSERT INTO koks VALUES($id + 1, '$full_link', '$short_link', '$hash')");
-            $s->execute();
-            $message = $short_link;
+            $message = 'error';
         }
-    }
-    else
-    {
-        $message = 'error';
     }
     echo json_encode($message);
 
